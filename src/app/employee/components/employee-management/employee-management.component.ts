b9a030830969e59bf161a9bd2fe9bc9fd40b6f27
Zafir <55599023+zafir100100@users.noreply.github.com>
2022-12-23 02:05:32 +0600

    state management done

--- src/app/employee/components/employee-management/employee-management.component.ts
+++ src/app/employee/components/employee-management/employee-management.component.ts
@@ -7,23 +7,45 @@ import { Router } from '@angular/router';
   templateUrl: './employee-management.component.html',
   styleUrls: ['./employee-management.component.css']
 })
+
 export class EmployeeManagementComponent {
   constructor(private fb: FormBuilder) {
 
   }
 
-  public form: FormGroup = this.fb.group({
+  step: number = 1;
+
+  public step1: FormGroup = this.fb.group({
     firstName: [null, [Validators.required, Validators.maxLength(20)]],
     lastName: [null, [Validators.required, Validators.maxLength(20)]],
     dob: [null, [Validators.required, Validators.maxLength(10)]],
     phone: [null, [Validators.required, Validators.maxLength(11)]],
     gender: [null, [Validators.required, Validators.maxLength(6)]],
   });
+  public step2: FormGroup = this.fb.group({
+    skillName: [null, [Validators.required, Validators.maxLength(50)]],
+    experience: [null, [Validators.required, Validators.max(60)]],
+    skillLevel: [null, [Validators.required, Validators.maxLength(12)]],
+  });
+  public step3: any = null;
 
   ngOnInit(): void {
   }
 
-  public submit(): void {
-    console.log(this.form.value);
+  public step1Next(): void {
+    this.step = 2;
+  }
+
+  public step2Next(): void {
+    this.step3 = {
+      ...this.step1.value,
+      ...this.step2.value,
+    };
+    console.log(this.step3);
+    this.step = 3;
+  }
+
+  public step3Next(): void {
+    console.log(this.step3);
   }
 }
